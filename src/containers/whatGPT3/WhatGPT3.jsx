54f04ef54a289fe15f9a4352787f9639c4d4b364
Aakash Shrestha <jackaakash4@gmail.com>
2023-04-10 23:25:02 +0545

    Header brand and features updated

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './WhatGPT3.css'
+import Features from '../../components/features/Features'
 
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div>
+      <div className="gpt3__whatgpt3 section_margin" id='wgpt3'>
+        <div className="gpt3__whatgpt3-feature">
+          <Features />
+        </div>
+        <div className="gpt3__whatgpt3-heading">
+          <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+          <p>Explore the library</p>
+        </div>
+        <div className="gpt3__whatgpt3-container">
+          <Features />
+          <Features />
+          <Features />
+        </div>
+      </div>
+    </div>
   )
 }
 
